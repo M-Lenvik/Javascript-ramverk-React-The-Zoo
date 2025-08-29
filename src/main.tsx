d25d9f9553f2e4d55860b3ee71fa0f0445d366df
M-Lenvik <lenvik.marie@gmail.com>
2025-08-29 16:57:29 +0200

    still trying to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
+import "./_index.scss";
 import App from "./App.tsx";
 import "./App.css";
 
